934b33a616738e9b6408ba8b96f675e010c292ca
Martin Graham <martin.anderson.graham@gmail.com>
2022-03-22 12:49:28 -0500

    Updated message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 31234;
 const SERVER = HTTP.createServer( (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.write("Hello Sam!");
+  res.write("Hello Sterling!");
   res.end();
 });
 
