61842f7bd5ad45a6d03f47df88c501e82dd699d6
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-08-07 16:33:22 -0500

    Added navbar links and title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Navbar from "./navbar.svelte";
+</script>
+
+<style>
+  :global(:root) {
+    --nav-bg-color: blue;
+    --sidebar-width: 130px;
+    --title-bg-color: rgb(241, 128, 128);
+  }
+
+  :global(body) {
+    padding: 0;
+    margin: 0;
+  }
+
+  #navbar {
+    background-color: var(--nav-bg-color);
+    width: var(--sidebar-width);
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    min-height: 100%;
+  }
+
+  #title {
+    margin-left: var(--sidebar-width);
+    background-color: var(--title-bg-color);
+    width: 100%;
+    padding-top: 1%;
+    padding-bottom: .5%;
+    padding-left: 1%;
+  }
+
+  #footer {
+    position: absolute;
+    bottom: 0px;
+    left: var(--sidebar-width);
+    background-color: gray;
+    width: 100%;
+    padding: 5%
+    
+  }
+</style>
+
+<div id="title">
+  <h1>Welcome to martingraham.dev</h1>
+</div>
+<div id="navbar">
+  <Navbar />
+</div>
+
+<div id="footer">
+    Icons courtesy of <a href="icons8.com">icons8</a>
+</div>
---
+++ src/routes/navbar.svelte
@@ -0,0 +1,105 @@
+<ul>
+  <li class="container">
+		<a target="_blank" href="google.com">
+			<img class="image" src="/images/projects.png" alt="My Projects" />
+			<div class="overlay">
+				<div class="text">projects</div>
+			</div>
+		</a>
+	</li>
+
+	<li class="container">
+		<a target="_blank" href="https://github.com/martin-anderson-graham">
+			<img class="image" src="/images/gh.png" alt="My Github Account" />
+			<div class="overlay">
+				<div class="text">github</div>
+			</div>
+		</a>
+	</li>
+
+  <li class="container">
+		<a target="_blank" href="/resume.pdf">
+			<img class="image" src="/images/resume.png" alt="My Resume" />
+			<div class="overlay">
+				<div class="text">resume</div>
+			</div>
+		</a>
+	</li>
+
+	<li class="container">
+		<a target="_blank" href="https://dev.to/martinandersongraham">
+			<img class="image" src="/images/blog.png" alt="My dev.to blog" />
+			<div class="overlay">
+				<div class="text">blog</div>
+			</div>
+		</a>
+	</li>
+
+	<li class="container">
+		<a target="_blank" href="https://twitter.com/theCodingMartin">
+			<img class="image" src="/images/twitter.png" alt="My Twitter Account" />
+			<div class="overlay">
+				<div class="text">twitter</div>
+			</div>
+		</a>
+	</li>
+
+
+
+</ul>
+
+<style>
+	ul {
+		list-style-type: none;
+    
+    width: auto;
+		/* margin: 1%; */
+		padding-left: 10%;
+	}
+
+  li {
+    padding: 10%;
+  }
+
+	.container {
+		position: relative;
+		width: 50%;
+	}
+
+	.image {
+		display: block;
+		width: 100%;
+		height: auto;
+	}
+
+	.overlay {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		height: 100%;
+		width: 100%;
+		opacity: 0;
+		transition: 0.5s ease;
+		background-color: var(--nav-bg-color);
+	}
+
+	.container:hover .overlay {
+		opacity: 1;
+	}
+
+	.text {
+		color: white;
+		font-size: 20px;
+
+	  position: absolute;
+    
+		top: 50%;
+		left: 50%;
+		-webkit-transform: translate(-50%, -50%);
+		-ms-transform: translate(-50%, -50%);
+		transform: translate(-50%, -50%);
+		text-align: center;
+	}
+</style>
