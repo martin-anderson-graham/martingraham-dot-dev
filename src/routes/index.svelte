c0e2d875092c98e3f02930cde02771f32bd30d3a
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-08-11 15:22:08 -0500

    updated layouts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,80 +1,10 @@
 <script>
-  import Navbar from "./navbar.svelte";
+	import Navbar from './navbar.svelte';
+  import About from './about.svelte';
+  import Footer from './footer.svelte';
 </script>
 
-<style>
-  :global(:root) {
-    --color-1: #002642;
-    --color-2: #840032;
-    --color-3: #E59500;
-    --color-4: #E5DADA;
-    --color-5: #02040F;
+<Navbar />
+<About />
+<Footer />
 
-
-    --sidebar-width: 130px;
-    --nav-bg-color: var(--color-1);
-    --title-bg-color: var(--color-2);
-    --title-text-color: var(--color-4);
-    --body-bg-color: var(--color-4);
-    --footer-bg-color: var(--color-5);
-    --footer-text-color: var(--color-3);
-  }
-
-  :global(body) {
-    padding: 0;
-    margin: 0;
-    font-family: 'Cabin', sans-serif;
-    background-color: var(--body-bg-color);
-
-  }
-
-  #navbar {
-    background-color: var(--nav-bg-color);
-    width: var(--sidebar-width);
-    position: fixed;
-    top: 0px;
-    left: 0px;
-    min-height: 100%;
-  }
-
-  #title {
-    margin-left: var(--sidebar-width);
-    background-color: var(--title-bg-color);
-    color: var(--title-text-color);
-    width: 100%;
-    padding-top: 1%;
-    padding-bottom: .5%;
-    padding-left: 1%;
-  }
-
-  #footer {
-    position: absolute;
-    bottom: 0px;
-    left: var(--sidebar-width);
-    background-color: var(--footer-bg-color);
-    color: var(--footer-text-color);
-    width: 100%;
-    padding: 2%
-  }
-
-  #footer > p > a {
-    color: var(--footer-text-color)
-  }
-</style>
-
-<div id="title">
-  <h1>martingraham.dev</h1>
-</div>
-<div id="navbar">
-  <Navbar />
-</div>
-
-<div>
-  <p>Content</p>
-</div>
-
-<div id="footer">
-    <p>
-      Icons courtesy of <a href="icons8.com">icons8</a>
-    </p>
-</div>
---
+++ src/routes/projects.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Navbar from './navbar.svelte';
+	import About from './about.svelte';
+	import Footer from './footer.svelte';
+</script>
+
+<Navbar />
+<div class="content-container">
+  <h1 id="project-header">A sample of my projects</h1>
+  <hr>
+    <a target="_blank" href="https://github.com/martin-anderson-graham/martingraham-dot-dev"><h2>martingraham.dev</h2></a>
+    This website is a <a target="_blank" href="https://kit.svelte.dev/">SvelteKit</a> application hosted on a <a target="_blank" href = "https://www.linode.com">Linode</a> VPS.  I use it as a playground for testing new technology and new-to-me techniques - for instance using <a target="_blank" href="https://github.com/martin-anderson-graham/martingraham-dot-dev/blob/main/.github/workflows/update-linode.yml">Github Actions</a> to automate deploying and building this site to production.
+  <hr>
+
+</div>
+<Footer />
+
+<style>
+  #project-header {
+    color: var(--text-emphasis-color);
+  }
+</style>
