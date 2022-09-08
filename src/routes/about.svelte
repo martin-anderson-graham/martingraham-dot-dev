281454d8ac69b763bb9611cee68dc4f43a0a05e0
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-09-08 10:40:20 -0500

    fixed another about-me typo

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,7 @@
 <div class="content-container">
 	<h1 id="about-title"><strong>Good day, my name is Marty.</strong></h1>
 	<p id="about-content">
-    I'm a software engineer located in wonderful <a target="_blank" href = "https://en.wikipedia.org/wiki/Waco,_Texas"> Waco Texas</a>.  I enjoy back-end programming, as making things work has always been satisfying to me.  I was a teacher and solo IT department in a small school for over a decade.  In my spare time I like to tinker with microcontrollers (ask me about my <a target="_blank" href="https://dan-in-ca.github.io/SIP/">drip irrigation system</a>), tend to my beehives (ask me about resisting the urge to run away from bees) and play table tennis (ask me about how it is <a target = "_blank" href="https://www.ittf.com/handbook/">not the same</a> as ping pong)
+    I'm a software engineer located in wonderful <a target="_blank" href = "https://en.wikipedia.org/wiki/Waco,_Texas"> Waco Texas</a>.  I enjoy back-end programming, as making things work has always been satisfying to me.  I was a teacher and solo IT department in a small school for over a decade.  In my spare time I like to tinker with microcontrollers (ask me about my <a target="_blank" href="https://dan-in-ca.github.io/SIP/">drip irrigation system</a>), tend to my beehives (ask me about resisting the urge to run away from bees) and play table tennis (ask me about how it is <a target = "_blank" href="https://www.ittf.com/handbook/">not the same</a> as ping pong).
   </p>
 </div>
 
